6f5e49c314d3775ae6fade57855642effc28b85f
app_lilly_welles <libapps@iu.edu>
2025-08-05 13:23:53 -0400

    Changed to use aira-label consistent with best web practices for accessibility.

--- plugins/ShortcodeVideo/views/public/javascripts/quality.js
+++ plugins/ShortcodeVideo/views/public/javascripts/quality.js
@@ -23,8 +23,8 @@ Object.assign(MediaElementPlayer.prototype, {
 	buildquality: function buildquality(player, controls, layers, media) {
 
 		var t = this,
-		    qualities = [],
-		    children = t.mediaFiles ? t.mediaFiles : t.node.children;
+			qualities = [],
+			children = t.mediaFiles ? t.mediaFiles : t.node.children;
 
 		for (var i = 0, total = children.length; i < total; i++) {
 			var s = children[i];
@@ -43,7 +43,7 @@ Object.assign(MediaElementPlayer.prototype, {
 		t.cleanquality(player);
 
 		var qualityTitle = mejs.Utils.isString(t.options.qualityText) ? t.options.qualityText : mejs.i18n.t('mejs.quality-quality'),
-		    getQualityNameFromValue = function getQualityNameFromValue(value) {
+			getQualityNameFromValue = function getQualityNameFromValue(value) {
 			var label = void 0;
 			if (value === 'auto') {
 				var src = qualities[0];
@@ -54,27 +54,27 @@ Object.assign(MediaElementPlayer.prototype, {
 
 			return label;
 		},
-		    defaultValue = getQualityNameFromValue(t.options.defaultQuality);
+			defaultValue = getQualityNameFromValue(t.options.defaultQuality);
 
 		player.qualitiesButton = document.createElement('div');
 		player.qualitiesButton.className = t.options.classPrefix + 'button ' + t.options.classPrefix + 'qualities-button';
-		player.qualitiesButton.innerHTML = '<button type="button" aria-controls="' + t.id + '" title="' + qualityTitle + '" ' + ('aria-label="' + qualityTitle + '" tabindex="0">' + defaultValue + '</button>') + ('<div class="' + t.options.classPrefix + 'qualities-selector ' + t.options.classPrefix + 'offscreen">') + ('<ul class="' + t.options.classPrefix + 'qualities-selector-list"></ul>') + '</div>';
+	   player.qualitiesButton.innerHTML = '<button type="button" aria-controls="' + t.id + '" title="' + qualityTitle + '" ' + ('aria-label="' + defaultValue + '" tabindex="0">' + defaultValue + '</button>') + ('<div class="' + t.options.classPrefix + 'qualities-selector ' + t.options.classPrefix + 'offscreen">') + ('<ul class="' + t.options.classPrefix + 'qualities-selector-list"></ul>') + '</div>';
 
 		t.addControlElement(player.qualitiesButton, 'qualities');
 
 		for (var _i = 0, _total = qualities.length; _i < _total; _i++) {
 			var src = qualities[_i],
-			    quality = src instanceof HTMLElement ? src.getAttribute('data-quality') : src['data-quality'],
-			    inputId = t.id + '-qualities-' + quality;
+				quality = src instanceof HTMLElement ? src.getAttribute('data-quality') : src['data-quality'],
+				inputId = t.id + '-qualities-' + quality;
 
 			player.qualitiesButton.querySelector('ul').innerHTML += '<li class="' + t.options.classPrefix + 'qualities-selector-list-item">' + ('<input class="' + t.options.classPrefix + 'qualities-selector-input" type="radio" name="' + t.id + '_qualities"') + ('disabled="disabled" value="' + quality + '" id="' + inputId + '"  ') + ((quality === defaultValue ? ' checked="checked"' : '') + '/>') + ('<label for="' + inputId + '" class="' + t.options.classPrefix + 'qualities-selector-label') + ((quality === defaultValue ? ' ' + t.options.classPrefix + 'qualities-selected' : '') + '">') + ((src.title || quality) + '</label>') + '</li>';
 		}
 
 		var inEvents = ['mouseenter', 'focusin'],
-		    outEvents = ['mouseleave', 'focusout'],
-		    radios = player.qualitiesButton.querySelectorAll('input[type="radio"]'),
-		    labels = player.qualitiesButton.querySelectorAll('.' + t.options.classPrefix + 'qualities-selector-label'),
-		    selector = player.qualitiesButton.querySelector('.' + t.options.classPrefix + 'qualities-selector');
+			outEvents = ['mouseleave', 'focusout'],
+			radios = player.qualitiesButton.querySelectorAll('input[type="radio"]'),
+			labels = player.qualitiesButton.querySelectorAll('.' + t.options.classPrefix + 'qualities-selector-label'),
+			selector = player.qualitiesButton.querySelector('.' + t.options.classPrefix + 'qualities-selector');
 
 		for (var _i2 = 0, _total2 = inEvents.length; _i2 < _total2; _i2++) {
 			player.qualitiesButton.addEventListener(inEvents[_i2], function () {
@@ -95,7 +95,7 @@ Object.assign(MediaElementPlayer.prototype, {
 			radio.disabled = false;
 			radio.addEventListener('click', function () {
 				var self = this,
-				    newQuality = self.value;
+					newQuality = self.value;
 
 				var selected = player.qualitiesButton.querySelectorAll('.' + t.options.classPrefix + 'qualities-selected');
 				for (var _i5 = 0, _total5 = selected.length; _i5 < _total5; _i5++) {
@@ -113,7 +113,7 @@ Object.assign(MediaElementPlayer.prototype, {
 				var currentTime = media.currentTime;
 
 				var paused = media.paused,
-				    canPlayAfterSourceSwitchHandler = function canPlayAfterSourceSwitchHandler() {
+					canPlayAfterSourceSwitchHandler = function canPlayAfterSourceSwitchHandler() {
 					if (!paused) {
 						media.setCurrentTime(currentTime);
 						media.play();
@@ -123,7 +123,7 @@ Object.assign(MediaElementPlayer.prototype, {
 
 				for (var _i6 = 0, _total7 = qualities.length; _i6 < _total7; _i6++) {
 					var _src = qualities[_i6],
-					    _quality = _src instanceof HTMLElement ? _src.getAttribute('data-quality') : _src['data-quality'];
+						_quality = _src instanceof HTMLElement ? _src.getAttribute('data-quality') : _src['data-quality'];
 
 					if (_quality === newQuality) {
 						player.qualitiesButton.querySelector('button').innerHTML = _src.title || getQualityNameFromValue(newQuality);
@@ -142,7 +142,7 @@ Object.assign(MediaElementPlayer.prototype, {
 				var radio = mejs.Utils.siblings(this, function (el) {
 					return el.tagName === 'INPUT';
 				})[0],
-				    event = mejs.Utils.createEvent('click', radio);
+					event = mejs.Utils.createEvent('click', radio);
 				radio.dispatchEvent(event);
 			});
 		}
